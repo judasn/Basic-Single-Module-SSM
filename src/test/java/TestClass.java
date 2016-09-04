2a57cbf7e3bfc0e35ee4facb227f203b4ffcbda1
yangkun38 <yangkun7@jd.com>
2016-09-04 10:15:50 +0800

    添加测试类

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,17 @@
+import sun.applet.Main;
+
+/**
+ * Created by jerry on 2016/9/4.
+ */
+public class TestClass {
+
+    public static void main(String argv[]){
+        System.out.println("hello world!");
+        for (int i = 0; i <20 ; i++) {
+            System.out.println("tom jerry" + i);
+            i
+        }
+
+    }
+
+}
