0800c3236c4112692900033463ae3244fc5229e9
yangkun38 <yangkun7@jd.com>
2016-09-04 10:18:12 +0800

    添加修改

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -9,7 +9,7 @@ public class TestClass {
         System.out.println("hello world!");
         for (int i = 0; i <20 ; i++) {
             System.out.println("tom jerry" + i);
-            i
+
         }
 
     }
